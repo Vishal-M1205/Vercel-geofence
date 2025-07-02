d5b0cfd20c6409c0c239ae31f102f14192879906
Vishal-M1205 <157288502+Vishal-M1205@users.noreply.github.com>
2025-07-02 23:16:38 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -106,6 +106,6 @@ def google_form():
     </html>
     """)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local dev
-    app.run(host='0.0.0.0', port=port)
+def handler(environ, start_response):
+    return app(environ, start_response)
+

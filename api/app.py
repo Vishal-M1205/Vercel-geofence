a50a44edd68223d1a550cc952414adef51b29da9
Vishal-M1205 <157288502+Vishal-M1205@users.noreply.github.com>
2025-07-11 11:36:45 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -85,7 +85,7 @@ def google_form():
                         if (distance <= maxDistance) {{
                             document.getElementById('form-container').innerHTML = `
                                 <iframe 
-                                    src="https://forms.gle/FN6xqWsgZACJUBEM7"
+                                    src="https://forms.gle/fa4AGmdqjZVm9Zon8"
                                     width="640" height="520" frameborder="0">
                                     Loading…
                                 </iframe>`;

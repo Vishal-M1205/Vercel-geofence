165440beea3927c7a31d98eed2bfc47e08a0b954
Vishal Manivannan <157288502+Vishal-M1205@users.noreply.github.com>
2026-01-26 17:45:35 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -85,7 +85,7 @@ def google_form():
                         if (distance <= maxDistance) {{
                             document.getElementById('form-container').innerHTML = `
                                 <iframe 
-                                    src="https://forms.gle/mdqHz11pJNoDJLF3A"
+                                    src="https://forms.gle/Kz2DwKW5dWYjU5k47"
                                     width="640" height="520" frameborder="0">
                                     Loading…
                                 </iframe>`;
@@ -124,6 +124,7 @@ def google_form():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
